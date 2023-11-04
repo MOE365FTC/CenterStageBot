c11a45ee5757c97a5a0380a206df3f3e29a0fb94
modulo88 <110422337+modulo88@users.noreply.github.com>
2023-11-04 11:49:28 -0400

    redLeftRR.java and blueLeftRR.java for autonomous with new pathing, blue and redCycle incomplete

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomus/redLeftRR.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.autonomus;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.MOEBot;
+import org.firstinspires.ftc.teamcode.rr.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.rr.trajectorysequence.TrajectorySequence;
+
+@Autonomous
+public class redLeftRR extends LinearOpMode {
+    MOEBot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        robot = new MOEBot(hardwareMap, gamepad1, gamepad2, telemetry);
+
+        Pose2d startPose = new Pose2d(-62,-12,0);
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence pixelLeft = drive.trajectorySequenceBuilder(startPose)
+                //vision
+                .lineToLinearHeading(new Pose2d(33,12, Math.toRadians(270)))
+                .waitSeconds(0.25)
+                //spike deposit
+                .lineToLinearHeading(new Pose2d(45,14, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(46,30), Math.toRadians(90))
+                .splineTo(new Vector2d(32,46), Math.toRadians(90))
+                .waitSeconds(0.25)
+                //pixel backdrop dispense
+                .build();
+
+        TrajectorySequence pixelCenter = drive.trajectorySequenceBuilder(startPose)
+                //vision
+                .lineToLinearHeading(new Pose2d(33,12, Math.toRadians(180)))
+                .waitSeconds(0.25)
+                //spike deposit
+                .lineToLinearHeading(new Pose2d(45,14, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(46,30), Math.toRadians(90))
+                .splineTo(new Vector2d(32,46), Math.toRadians(90))
+                .waitSeconds(0.25)
+                //pixel backdrop dispense
+                .build();
+
+        TrajectorySequence pixelRight = drive.trajectorySequenceBuilder(startPose)
+                //vision
+                .lineToLinearHeading(new Pose2d(33,12, Math.toRadians(90)))
+                .waitSeconds(0.25)
+                //spike deposit
+                .lineToLinearHeading(new Pose2d(45,14, Math.toRadians(90)))
+                .splineToConstantHeading(new Vector2d(46,30), Math.toRadians(90))
+                .splineTo(new Vector2d(32,46), Math.toRadians(90))
+                .waitSeconds(0.25)
+                //pixel backdrop dispense
+                .build();
+
+
+
+
+        TrajectorySequence redCycle = drive.trajectorySequenceBuilder(new Pose2d(-32, 46))
+                .lineTo(new Vector2d(-36, -60))
+                .lineTo(new Vector2d(-36, 17))
+                .splineTo(new Vector2d(-32,48), Math.toRadians(90))
+                .waitSeconds(0.25)
+                //pixel backdrop output
+                .lineTo(new Vector2d(-36, -60))
+                .lineTo(new Vector2d(-36, 17))
+                .splineTo(new Vector2d(-32,48), Math.toRadians(90))
+                .waitSeconds(0.25)
+                //pixel backdrop output
+                .strafeLeft(28)
+                .forward(10)
+                .build();
+
+        while(!isStarted() && !isStopRequested()) {
+            //init loop
+
+            robot.vision.detectProp();
+            telemetry.addData("Prop Pos", robot.vision.getPropPos());
+        }
+
+        waitForStart();
+        robot.vision.stopDetecting();
+
+
+    }
+}
